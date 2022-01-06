fe8344cddc4b84a75e1d95556cd4ca829e6dc7a7
Charles Wilkins <psychw@exmail.nottingham.ac.uk>
2022-01-06 13:52:24 +0000

    Add skeleton code for solver

--- src/WorkflowSatisfiability.py
+++ src/WorkflowSatisfiability.py
@@ -1,6 +1,8 @@
-import sys
-import os
+import sys # Used for command-line arguments
+import os # Used for file paths
+from time import time
 from enum import Enum
+from ortools.sat.python import cp_model
 
 class ConstraintType(Enum):
     Authorisation = 0
@@ -50,7 +52,7 @@ def readFile(fileName):
             constraintValues = list(map(int, map(lambda word: word[1], words)))
             constraint = Constraint(ConstraintType.Authorisation, constraintValues)
             constraints.append(constraint)
-            
+
         elif constraintType == "separation-of-duty":
             words.remove(constraintType)
             constraintValues = list(map(int, map(lambda word: word[1], words)))
@@ -70,11 +72,38 @@ def readFile(fileName):
     instance = Instance(numberOfSteps, numberOfUsers, numberOfConstraints, constraints)
     return instance
 
+def solve(instance):
+    print("Building model")
+    model = cp_model.CpModel()
+    
+    #Define variables
+
+    #Define constraints
+
+    #Solve
+    print("Solving instance")
+    starttime = int(time() * 1000)
+    solver = cp_model.CpSolver()
+    status = solver.Solve(model)
+    endtime = int(time() * 1000)
+    print(f"Instance solved in {endtime - starttime}ms")
+    if status == cp_model.OPTIMAL or status == cp_model.FEASIBLE:
+        print("sat")
+        printModel(solver)
+    else:
+        print("unsat")
+
+def printModel(solver):
+    #TODO print
+    print("TODO print")
+
 if __name__ == "__main__":
     if len(sys.argv) >= 2:
         file = sys.argv[1]
         print(f"File selected: {file}")
+        print("Reading file")
         instance = readFile(file)
-        print(instance.numberOfUsers, instance.numberOfTasks, instance.numberOfConstraints, len(instance.constraints))
+        print("Starting solver")
+        solve(instance)
     else:
         print("Please select a problem instance file")
